e151e64b6a6d390c3b7ec5ac6e2fd6c47cda202b
Tatiana-E <tanya.era1989@gmail.com>
2024-07-13 15:51:04 +0500

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,8 @@
+my_dict={'Mihail': 2014, 'Yaroslav': 2008, 'Valeriy': 1985, 'Taniana': 1989}
+print(my_dict)
+print(my_dict.get('Mihail'))
+print(my_dict.get('Lidiya'))
+my_dict.update({'Gregory': 2023, 'Miroslava': 2019})
+del my_dict['Taniana']
+print(my_dict)
+
